242e67831543fda2a86890acbd3dca8609d4eaa0
davidenox <nocedav31@gmail.com>
2024-10-21 12:17:49 +0200

    Adding Es4 and Es5

---
+++ Es5/prova.m
@@ -0,0 +1,11 @@
+% Codice di run
+A = [2, 1; 2, -2];  % Matrice del sistema
+b = [3; 0];         % Vettore dei termini noti
+x0 = [0; 0];       % Stima iniziale
+epsilon = 1e-5;    % Soglia di precisione
+N_max = 6;         % Numero massimo di iterazioni
+
+[x, K, r_norm] = gauss_seidelMethod(A, b, x0, epsilon, N_max);  % Chiamata alla funzione
+disp('Soluzione x:'); disp(x);        % Visualizza la soluzione
+disp(['Numero di iterazioni K: ', num2str(K)]);  % Visualizza il numero di iterazioni
+disp(['Norma del residuo: ', num2str(r_norm)]);    % Visualizza la norma del residuo
