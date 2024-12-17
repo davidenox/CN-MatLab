1c57c16f8fdece8961fb7882ee4ea7340b9bb5f7
davidenox <nocedav31@gmail.com>
2024-12-17 12:21:37 +0100

    vault backup: 2024-12-17 12:21:37

--- Es1/prova.m
+++ Es1/prova.m
@@ -2,6 +2,6 @@ x = [0, 1, 2, 3];
 y = [0, 3, 1, 1];
 t = 2.3;
 
-result = interpolaRuffiniHornerMatrix2Es1(x, y, t);
+result = interpolaRuffiniHornerMatrixEs1(x, y, t);
 disp('Risultato interpolato:');
 disp(result);
